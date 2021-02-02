668f0344c41f12e5a7e14db3d08b4f01d6933e41
Triusdeil <triusdeil@gmail.com>
2021-02-02 00:22:26 -0400

    subida de imagenes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const multer = require('multer');
 const morgan = require('morgan');
-const exphbs = require('express-handlebars');
 const path = require('path');
 const { diskStorage } = require('multer');
 
@@ -11,25 +10,20 @@ require('./database');
 
 //Settings
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'views'));
-app.engine('.hbs', exphbs({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views') + 'layouts'),
-    partialsDir: path.join(app.get('views') + 'partials'),
-}))
-app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'ejs');
 
 //Middleware
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-multer.diskStorage({
-    destination: path.join(__dirname,'public/uploads'),
-    filename:(req,res,cb) =>{
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename:(req, file, cb) =>{
         cb(null, new Date().getTime() + path.extname(file.originalname));
     }
 })
-app.use(multer().single('image'));
+app.use(multer({storage}).single('image'));
 
 //Routes
 app.use(require('./routes'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,35 @@
 const {Router} = require('express');
 const router = Router();
+const photo = require('../models/photo')
+const cloudinary = require('cloudinary');
+const fs = require('fs-extra');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 router.get('/', (req,res) =>{
-    res.send('Hello World');
+    res.render('images');
+})
+
+router.get('/images/add', (req,res) =>{
+    res.render('images_form')
+})
+
+router.post('/images/add',async (req,res)=>{
+    const {title, description} = req.body
+    const result = await cloudinary.v2.uploader.upload(req.file.path);
+    const newPhoto = new photo({
+        title : title,
+        description: description,
+        imageURL: result.url,
+        public_id: result.public_id
+    });
+    await newPhoto.save();
+    await fs.unlink(req.file.path);
+    res.send('received')
 })
 
 module.exports = router;
